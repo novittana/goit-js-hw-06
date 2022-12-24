0896526e3b1113fc5a5a9801f3a71930496bab3f
Tetiana Novitska <novittana2410@gmail.com>
2022-12-24 12:23:28 +0200

    init

--- js/task-06.js
+++ js/task-06.js
@@ -2,14 +2,13 @@ const userInputEl = document.querySelector("#validation-input");
 
 userInputEl.addEventListener("blur", (event) => {
   const minInpLength = Number(userInputEl.dataset.length);
-  let userInpLength = userInputEl.value.length;
+  let userInpLength = Number(userInputEl.value.length);
 
   let borderStyle;
-  if (userInpLength === minInpLength) {
-    console.log(userInpLength === minInpLength);
-    borderStyle = userInputEl.classList.add("valid");
-  } else {
+  if (userInpLength !== minInpLength) {
     borderStyle = userInputEl.classList.add("invalid");
+  } else {
+    borderStyle = userInputEl.classList.add("valid");
   }
-  borderStyle;
+  return borderStyle;
 });
